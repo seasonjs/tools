bc958630bedb7f7adbe4902f03025d2177226b46
gonglixin980926 <1480059245@qq.com>
2021-02-02 18:43:25 +0800

    fix: 异步队列函数

---
+++ src/asyncQueue/index.ts
@@ -0,0 +1,24 @@
+export class AsyncQueue {
+    private funcQueue = []
+    private isExec = false
+    add<T>({id, func}){
+        if(!this.isExec){
+            this.exec()
+        }
+        // 正在执行时，放进队列里
+        this.funcQueue.push(func)
+    }
+    private async exec(){
+        if(!this.funcQueue.length){
+            return
+        }
+
+        // 执行函数
+        const head = this.funcQueue.shift()
+        this.isExec = true
+        await head()
+        this.isExec = false
+
+        this.exec()
+    }
+}
