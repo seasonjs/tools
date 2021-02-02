a013b4bd5e1e1db67e065c841912e7e0b0499d86
yinghao.xiao <yinghao.xiao@shopee.com>
2021-02-02 13:41:31 +0800

    feat: Add useStateCallback hook

---
+++ src/hooks/useStateCallback.ts
@@ -0,0 +1,22 @@
+import { useState, useRef, useEffect, useCallback } from 'react'
+
+const useStateCallback = (initialState) => {
+  const [state, setState] = useState(initialState)
+  const cbRef = useRef(null)
+
+  const setStateCallback = useCallback((state, cb) => {
+    cbRef.current = cb
+    setState(state)
+  }, [])
+
+  useEffect(() => {
+    if (cbRef.current) {
+      cbRef.current(state)
+      cbRef.current = null
+    }
+  }, [state])
+
+  return [state, setStateCallback]
+}
+
+export default useStateCallback
