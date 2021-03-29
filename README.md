f5d3d0f6b6fe448477334fe76df34daab23e38e0
Cyberhan123 <255542417@qq.com>
2021-03-29 15:40:55 +0800

    feat: add new utils

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tools
 常用js工具类
+
+-   定期升级第三方依赖
+```
+yarn upgrade-interactive --latest
+```
---
+++ src/hooks/forwardRefStatics.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+import type { RefAttributes } from "react";
+import {forwardRef} from "react";
+import hoistNonReactStatics from 'hoist-non-react-statics';
+
+export function forwardRefWithStatics<P, T = any, S = {}>(
+    component: React.RefForwardingComponent<T, P>,
+    statics?: S
+): React.FunctionComponent<P & RefAttributes<T>> & S {
+    return hoistNonReactStatics(forwardRef(component), statics as any) as any;
+}
