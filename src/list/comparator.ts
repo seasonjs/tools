9c05166482d8c63414b252aaa90665272e034131
Cyberhan123 <255542417@qq.com>
2021-05-07 18:20:01 +0800

    feat: linkedList.ts

---
+++ src/list/comparator.ts
@@ -0,0 +1,83 @@
+//copy from https://github.com/trekhleb/javascript-algorithms/blob/master/src/utils/comparator/Comparator.js
+export default class Comparator {
+  private compare: (a: any, b: any) => 0 | 1 | -1;
+  /**
+   * @param {function(a: *, b: *)} [compareFunction] - It may be custom compare function that, let's
+   * say may compare custom objects together.
+   */
+  constructor(compareFunction: (a: any, b: any) => 0 | 1 | -1) {
+    this.compare = compareFunction || Comparator.defaultCompareFunction;
+  }
+
+  /**
+   * Default comparison function. It just assumes that "a" and "b" are strings or numbers.
+   * @param {(string|number)} a
+   * @param {(string|number)} b
+   * @returns {number}
+   */
+  static defaultCompareFunction(a: number, b: number) {
+    if (a === b) {
+      return 0;
+    }
+
+    return a < b ? -1 : 1;
+  }
+
+  /**
+   * Checks if two variables are equal.
+   * @param {*} a
+   * @param {*} b
+   * @return {boolean}
+   */
+  equal(a: any, b: undefined) {
+    return this.compare(a, b) === 0;
+  }
+
+  /**
+   * Checks if variable "a" is less than "b".
+   * @param {*} a
+   * @param {*} b
+   * @return {boolean}
+   */
+  lessThan(a: any, b: any) {
+    return this.compare(a, b) < 0;
+  }
+
+  /**
+   * Checks if variable "a" is greater than "b".
+   * @param {*} a
+   * @param {*} b
+   * @return {boolean}
+   */
+  greaterThan(a: any, b: any) {
+    return this.compare(a, b) > 0;
+  }
+
+  /**
+   * Checks if variable "a" is less than or equal to "b".
+   * @param {*} a
+   * @param {*} b
+   * @return {boolean}
+   */
+  lessThanOrEqual(a: any, b: undefined) {
+    return this.lessThan(a, b) || this.equal(a, b);
+  }
+
+  /**
+   * Checks if variable "a" is greater than or equal to "b".
+   * @param {*} a
+   * @param {*} b
+   * @return {boolean}
+   */
+  greaterThanOrEqual(a: any, b: undefined) {
+    return this.greaterThan(a, b) || this.equal(a, b);
+  }
+
+  /**
+   * Reverses the comparison order.
+   */
+  reverse() {
+    const compareOriginal = this.compare;
+    this.compare = (a, b) => compareOriginal(b, a);
+  }
+}
