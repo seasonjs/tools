c8e7f11faf7bba6ae40a3a66f6bd9dbb8e5e6670
Cyberhan123 <38837039+Cyberhan123@users.noreply.github.com>
2021-07-17 13:02:03 +0800

    Update .umirc.ts

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   base: '/tools',
-  publicPath: '/tools/',
+  publicPath: './',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // ssr: {},
   locales: [['zh-CN', '中文']],
