35d35dfba9365a35c79f936afa0f5335ac85c71f
gonglixin980926 <1480059245@qq.com>
2021-08-20 17:44:59 +0800

    feat: add renderFunc

---
+++ src/dataSubscription/index.ts
@@ -0,0 +1,20 @@
+import { computed } from 'mobx';
+
+/**
+ *
+ * @param state 需要监听的对象
+ * @param depsFunc 需要监听的属性
+ * @param func 属性变化后，需要做的操作
+ */
+export function renderFunc<T extends any[]>(
+  state: object,
+  depsFunc: (state: object) => T,
+  func: (oldVal: T | undefined, newVal: T) => void,
+) {
+  const deps = computed(() => depsFunc(state));
+  const observeDispose = deps.observe_(({ oldValue, newValue }) => {
+    if (oldValue !== newValue) {
+      func(oldValue, newValue);
+    }
+  });
+}
